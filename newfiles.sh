ff4ff0c8785fd7fe5b584215def6bc93a25e1875
Julio Sene França <julio.sene@gmail.com>
2016-02-26 14:49:43 -0300

    Update newfiles.sh

--- newfiles.sh
+++ newfiles.sh
@@ -2,5 +2,7 @@ USERNAME="videosync"
 inotifywait -m /home/$USERNAME/videos/ -e create -e moved_to -e close_write |
     while read path action file; do
         # echo "The file '$file' appeared in directory '$path' via '$action'"
-         if [ "$action" = "MOVED_TO" ]; then md5sum /home/$USERNAME/$file > /home/$USERNAME/.verify/$file;  fi
+         if [ "$action" = "MOVED_TO" ]; then md5sum /home/$USERNAME/videos/$file > /home/$USERNAME/.verify/$file; echo "/bin/bash /home/$USERNAME/verify.sh $file"|at -m now + 2min; fi
+#         if [ "$file" = "acabou.txt" ]; then echo "Terminou"; fi
+        # do something with the file
     done
