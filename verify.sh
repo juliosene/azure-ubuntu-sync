84d74928d5db69afe1b43be2b2fba64f6215a61d
Julio Sene França <julio.sene@gmail.com>
2016-02-26 14:48:59 -0300

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,3 +1,8 @@
+# First of all, install mail tools
+# apt-get install mailutils
 USERNAME="videosync"
-         if [ "`md5sum /home/$USERNAME/videos/$1`" = "`cat /home/$USERNAME/.verify/$1 | head -n1`" ]; then echo "terminou" >> /home/$USERNAME/.verify/$1 ; fi
-
+MAILTO="jufranca@microsoft.com"
+if [ "`md5sum /home/$USERNAME/videos/$1`" = "`cat /home/$USERNAME/.verify/$1 | head -n1`" ]; then
+   mail -s "Your file $1 was sent" $MAILTO < /home/$USERNAME/.verify/$1;
+   rm /home/$USERNAME/.verify/$1
+fi
