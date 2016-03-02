830c608c484e3298a5b89dc994608318a9553867
Julio Sene França <julio.sene@gmail.com>
2016-03-02 18:10:28 -0300

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,8 +1,18 @@
 # First of all, install mail tools
 # apt-get install mailutils
+#
 USERNAME="videosync"
-MAILTO="jufranca@microsoft.com"
+MAILTO="your@email.com"
+#
+filename=$(basename "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+#
 if [ "`md5sum /home/$USERNAME/videos/$1`" = "`cat /home/$USERNAME/.verify/$1 | head -n1`" ]; then
    mail -s "Your file $1 was sent" $MAILTO < /home/$USERNAME/.verify/$1;
+   if [ $extension = "mp4" ]; then
+      java -jar videosync.jar videosync zlnnVE3dw4OZygNePUsSpd7pmeUMWd5RFdZTTn2nsvo= $1 /home/$USERNAME/videos/ > .videourl.txt
+      mail -s "Your video $1 was published!" $MAILTO < .videourl.txt
+   fi
    rm /home/$USERNAME/.verify/$1
 fi
