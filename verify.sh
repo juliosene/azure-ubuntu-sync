598dd3193d80fc9c1a1b3a4ece993d85049e94db
Julio Sene França <julio.sene@gmail.com>
2016-03-03 15:56:16 -0300

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -9,10 +9,10 @@ extension="${filename##*.}"
 filename="${filename%.*}"
 #
 if [ "`md5sum /home/$USERNAME/videos/$1`" = "`cat /home/$USERNAME/.verify/$1 | head -n1`" ]; then
-   mail -s "Your file $1 was sent" $MAILTO < /home/$USERNAME/.verify/$1;
+   mail -s "Your file $1 was completely received" $MAILTO < /home/$USERNAME/.verify/$1;
    if [ $extension = "mp4" ]; then
-      java -jar videosync.jar videosync zlnnVE3dw4OZygNePUsSpd7pmeUMWd5RFdZTTn2nsvo= $1 /home/$USERNAME/videos/ > .videourl.txt
-      mail -s "Your video $1 was published!" $MAILTO < .videourl.txt
+      VIDEOLINK=`java -jar videosync.jar videosync zlnnVE3dw4OZygNePUsSpd7pmeUMWd5RFdZTTn2nsvo= $1 /home/$USERNAME/videos/`
+      cat /home/$USERNAME/mailtemplate.html | sed "s,##VIDEOLINK##,$VIDEOLINK,g;s,##VIDEONAME##,$1,g" | mail -s "$(echo -e "Your video $1 was published\nContent-Type: text/html")" $MAILTO
    fi
    rm /home/$USERNAME/.verify/$1
 fi
